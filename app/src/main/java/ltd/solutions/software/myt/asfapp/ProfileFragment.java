028f7c72843a584734c05efe488a456e43e2b15a
Marios Marneros <34173109+marnerosm@users.noreply.github.com>
2018-09-01 15:12:54 +0300

    Info
    Changed Profile Fragment to Info !! Try find a way to use imagebuttons in fragment to go to url

--- app/src/main/java/ltd/solutions/software/myt/asfapp/ProfileFragment.java
+++ app/src/main/java/ltd/solutions/software/myt/asfapp/ProfileFragment.java
@@ -6,12 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class ProfileFragment extends Fragment {
+
+    ImageButton iButton,fbButton;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        iButton=(ImageButton)getView().findViewById(R.id.img_button1);
+        fbButton=(ImageButton)getView().findViewById(R.id.img_button2);
 
         return inflater.inflate(R.layout.profile_fragment, null);
     }
