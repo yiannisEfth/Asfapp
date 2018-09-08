fff339fb7de573310914248ceef25e6d5ebfc1de
Marios Marneros <34173109+marnerosm@users.noreply.github.com>
2018-09-08 21:34:38 +0300

    Client List View and Classes List View
    Created add class activity aswell

---
+++ app/src/main/java/ltd/solutions/software/myt/asfapp/AddClassActivity.java
@@ -0,0 +1,13 @@
+package ltd.solutions.software.myt.asfapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddClassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_class);
+    }
+}
---
+++ app/src/main/java/ltd/solutions/software/myt/asfapp/ClientView_Activity.java
@@ -0,0 +1,13 @@
+package ltd.solutions.software.myt.asfapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClientView_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_view);
+    }
+}
