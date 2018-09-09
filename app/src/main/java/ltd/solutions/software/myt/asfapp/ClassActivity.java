12b9fd1e914a593fbcf7cb89336b623aa5968c09
Marios Marneros <34173109+marnerosm@users.noreply.github.com>
2018-09-09 12:24:14 +0300

    Created intent for add class

--- app/src/main/java/ltd/solutions/software/myt/asfapp/ClassActivity.java
+++ app/src/main/java/ltd/solutions/software/myt/asfapp/ClassActivity.java
@@ -1,5 +1,6 @@
 package ltd.solutions.software.myt.asfapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,8 +21,8 @@ public class ClassActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               Intent intent =new Intent(ClassActivity.this,AddClassActivity.class);
+               startActivity(intent);
             }
         });
     }
