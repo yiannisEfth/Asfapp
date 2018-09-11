1e3813f80eacbce7eecfbcfa34d0e1ee5036fd5b
Theodoros Marinos <t-marinos@hotmail.com>
2018-09-11 16:20:37 +0300

    Completed Registration
    Only compare doesnt work

--- app/src/main/java/ltd/solutions/software/myt/asfapp/User.java
+++ app/src/main/java/ltd/solutions/software/myt/asfapp/User.java
@@ -3,13 +3,15 @@ package ltd.solutions.software.myt.asfapp;
 public class User {
     public String name , surname , email , phone;
     public int id;
+    public boolean isActive;
 
-    public User(String name , String surname , String email , String phone , int id) {
+    public User(String name , String surname , String email , String phone , int id, boolean status) {
         this.name = name;
         this.surname = surname ;
         this.email = email;
         this.phone = phone;
         this.id = id;
+        this.isActive = status;
     }
 
 }
