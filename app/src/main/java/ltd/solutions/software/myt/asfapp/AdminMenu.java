86bb252c713bc9ffd7e6b9c2272f9d58cd7bc175
Theodoros Marinos <tm00591@surrey.ac.uk>
2018-10-29 22:07:57 +0000

    fixed minor bugs

--- app/src/main/java/ltd/solutions/software/myt/asfapp/AdminMenu.java
+++ app/src/main/java/ltd/solutions/software/myt/asfapp/AdminMenu.java
@@ -33,7 +33,6 @@ public class AdminMenu extends AppCompatActivity {
             public void onClick(View v) {
                 Intent classes = new Intent(AdminMenu.this, ClassActivity.class);
                 startActivity(classes);
-                finish();
             }
         });
 
